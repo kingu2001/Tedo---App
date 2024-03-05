df37411fedc347319038f25b8c46e087d7848145
KT. Nguyen <kiettn12@gmail.com>
2024-03-05 10:10:55 +0100

    Designing the tablet app

---
+++ app/src/main/java/com/example/tedo/MVVM/Model/TestDocument.kt
@@ -0,0 +1,19 @@
+package com.example.tedo.MVVM.Model
+
+class TestDocument{
+    var Name : String
+        get() {
+            TODO()
+        }
+        set(value) {}
+    var Introduction :  String
+        get() {
+            TODO()
+        }
+        set(value) {}
+    var DocumentSupplied : String
+        get() {
+            TODO()
+        }
+        set(value) {}
+}
